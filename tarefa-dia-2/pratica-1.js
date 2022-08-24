9ab21580e1ae622ec6a4eed6112ff54fde9b5ba0
jorbsongiovanni <jorbson.giovanni@unesp.br>
2022-08-24 15:38:19 -0300

    mensagem de commit1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,3 +9,11 @@ seuemail = "jorbson.giovanni@unesp.br"
 suaprofissao = "Analista"
 suaidade = 51
 olugarquevocesonhaumdiaconhecer = "Italia"
+
+console.log(seunome)
+console.log(seuemail)
+console.log(suaprofissao)
+console.log(suaidade)
+console.log(olugarquevocesonhaumdiaconhecer)
+
+
