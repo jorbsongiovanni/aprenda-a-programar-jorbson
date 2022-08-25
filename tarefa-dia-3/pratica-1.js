e0af3c547467766306ea6e80e6492455ff337a10
jorbsongiovanni <jorbson.giovanni@unesp.br>
2022-08-25 17:48:44 -0300

    mensagem de commit1

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,9 @@
+
+
+let i = 1
+let n = 9 // imput do ususario
+
+while (i <= n) {
+    console.log(i)
+    i=i+1
+}
---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,13 @@
+
+// Tabuada
+
+let multiplica = 8 // tabuada do
+let n = 10 //multiplicador de 1 a n
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplica * i
+    let exibirTabuada = `${multiplica} X ${i} = ${resultado}`
+    console.log(exibirTabuada)
+    i=i+1
+}
